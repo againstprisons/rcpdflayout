0b076525467c3101965179b3f42fb12f15802690
Iris System <iris@iris.ac.nz>
2021-10-14 14:28:36 +1300

    object/text_segment: add attribute support

--- lib/rcpdflayout/object/text_segment.rb
+++ lib/rcpdflayout/object/text_segment.rb
@@ -14,6 +14,9 @@ module RcPdfLayout
       # The color to render this text segment in
       # @return [String]
       attr_accessor :color
+      # Attributes to add to the text segment ("bold", "italic", or "underline")
+      # @return [Array<String>]
+      attr_accessor :attributes
       # The text content of this segment
       # @return [String]
       attr_accessor :text
@@ -25,6 +28,7 @@ module RcPdfLayout
         @font_name = opts.fetch(:font, 'DeJaVu-Sans')
         @font_size = opts.fetch(:font_size, 12)
         @color = opts.fetch(:color, 'black')
+        @attributes = opts.fetch(:attributes, [])
         @text = text
       end
 
@@ -34,7 +38,22 @@ module RcPdfLayout
       # @return [MiniMagick::Image] Rendered image
       def render_final(opts = {})
         # Get our PPI
-        ppi = opts.fetch(:force_ppi, opts[:parent].ppi).to_f
+        ppi = opts.fetch(:force_ppi, opts[:parent]&.ppi).to_f
+
+        # If our text is exclusively whitespace, then replace it all with a
+        # single non-breaking space character (u00A0)
+        text = @text
+        text = "\u00A0" if @text.strip.empty?
+
+        # Grab our font into a local variable so we can modify it to handle
+        # attributes if needed
+        font_name = @font_name.dup
+
+        # Handle attribute: bold
+        font_name = "#{font_name}-Bold" if @attributes.include?('bold')
+
+        # Handle attribute: italic
+        font_name = "#{font_name}-Oblique" if @attributes.include?('italic')
 
         # Create an image with our text in it
         @image = self.class.create_image do |mg|
@@ -42,14 +61,23 @@ module RcPdfLayout
 
           mg.background('transparent')
           mg.fill(@color)
-          mg.font(@font_name)
+          mg.font(font_name)
           mg.pointsize(@font_size)
 
-          mg << "label:#{@text}"
+          mg << "label:#{text}"
           mg.repage('0x0')
         end
 
-        # Convert pixel size into millimebers
+        # Handle attribute: underline
+        if @attributes.include?('underline')
+          @image.combine_options do |mg|
+            mg.background(@color)
+            mg.gravity('south')
+            mg.splice("0x#{(@image.height.to_i / 15).to_i}")
+          end
+        end
+
+        # Convert pixel size into millimeters
         @size_mm = [@image.width, @image.height]
                    .map { |px| (px.to_i / ppi) / RcPdfLayout::MM_TO_INCH }
 
